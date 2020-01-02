1c1b46f9b30eeb1887a8ace3ccc4524a12db250b
DanielGeek <daniel30081990@gmail.com>
2020-01-02 13:10:05 -0300

    Letura y creacion de entidades
    funcion para obtener o crear entidades por c.c creaEHdetectEntities/:hosp_id
    obtener entidades por c.c
    getEpicrisisHospitalizadosByHospId
    obtener datos de epicrisis_hospitalizados
    getEpicrisisHospitalizados

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
 const router = Router();
 
-// llamamos la funcion para obtener los datos de la tabla epicrisis_hospitalizados
-const { getEpicrisisHospitalizados } = require('../controllers/index.controller');
+// incluimos las funciones del controlador
+const { getEpicrisisHospitalizados, getEpicrisisHospitalizadosByHospId, creaEHdetectEntities } = require('../controllers/index.controller');
 
-//ruta para obtener datos de los pacientes en la tabla epicrisis_hospitalizados
-router.get('/epicrisis_hospitalizados', getEpicrisisHospitalizados );
-//metodo para insert
-// router.post('/create_DetectEntities', createDetectEntities);
+//metodo para obtener los 100 primeros datos de epicrisis_hospitalizados
+router.get('/getEpicrisisHospitalizados', getEpicrisisHospitalizados);
+
+// metodo para obtener los datos de epicrisis_hospitalizados por hosp_id
+router.get('/getEpicrisisHospitalizadosByHospId/:hosp_id', getEpicrisisHospitalizadosByHospId);
+
+//ruta para obtener datos de los pacientes en la tabla epicrisis_hospitalizados, analizarlos y insertarlos
+router.get('/creaEHdetectEntities/:hosp_id', creaEHdetectEntities);
+
+//metodo para obtener los datos insertados con creaEHdetectEntitiesV2
+// router.post('/getDetectEntities', getDetectEntities);
 
 
 module.exports = router;
