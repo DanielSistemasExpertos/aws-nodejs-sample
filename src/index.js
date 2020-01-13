8941e0b5e361657f3ea194d283647ee7b23dc8e0
DanielGeek <daniel30081990@gmail.com>
2020-01-13 12:11:54 -0300

    Agregado motor de plantilla ejs
    se agrego ejs para tener un front-end y hacer busqueda de epicrisis, cuenta corriente asociada a epicrisis y analizar epicrisis pasando la cuenta corriente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+
+// configuraciones
+app.set('views', path.join(__dirname, 'views'));
+// uso de motor de plantilla
+app.set('view engine', 'ejs');
 
 // middlewares
 app.use(express.json());
@@ -8,5 +15,5 @@ app.use(express.urlencoded({extended: false}));
 // routes
 app.use(require('./routes/index'));
 
-app.listen(3000);
-console.log('Servidor en puerto 3000');
+app.listen(3002);
+console.log('Servidor en puerto 3002');
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,24 @@ const router = Router();
 // incluimos las funciones del controlador
 const { getEpicrisisHospitalizados, getEpicrisisHospitalizadosByHospId, creaEHdetectEntities } = require('../controllers/index.controller');
 
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+// al dar click en el boton buscar cuenta corrriente muestra el formulario para buscar cuenta corriente asociada al medico que atendio el paciente
+router.get('/epicrisis/', (req, res) => {
+    cuentaCC = req;
+    console.log(cuentaCC);
+    res.render('epicrisis/buscaCC');
+});
+
+// al dar click en el boton analizar con comprehend medical, muestra el formulario para buscar cuenta corriente asociada al medico que atendio el paciente y analizar el epicrisis con comprehend medical
+router.get('/epicrisis/creaComprehendMedical', (req, res) => {
+    cuentaCC = req;
+    console.log(cuentaCC);
+    res.render('epicrisis/creaComprehendMedical');
+});
+
 //metodo para obtener los 100 primeros datos de epicrisis_hospitalizados
 router.get('/getEpicrisisHospitalizados', getEpicrisisHospitalizados);
 
